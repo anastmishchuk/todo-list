828f6e9996e2160a31902564c0c454c73b43d2d2
Anastasiia Mishchuk <mishchuk.anastasiia@gmail.com>
2024-04-24 17:31:22 +0200

    Add field is_done to task

--- todo/admin.py
+++ todo/admin.py
@@ -5,8 +5,8 @@ from todo.models import Tag, Task
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ["created_at", "deadline", "tag"]
-    list_filter = ["created_at", "deadline"]
+    list_display = ["created_at", "deadline", "is_done"]
+    list_filter = ["created_at", "is_done"]
     search_fields = ["content", ]
 
 
--- todo/models.py
+++ todo/models.py
@@ -15,10 +15,11 @@ class Task(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField(default=False)
     tag = models.ManyToManyField(Tag, related_name='tasks')
 
     class Meta:
-        ordering = ("-created_at", )
+        ordering = ("-is_done", "-created_at")
 
     def __str__(self):
         return self.content
